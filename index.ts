4a370026515e8aada081c04dc45ebcdc58fdf9b6
lukeanders70 <luke.larson@berkeley.edu>
2023-01-02 22:31:14 -0800

    serve from build

--- index.ts
+++ index.ts
@@ -1,16 +1,39 @@
 import * as express from 'express';
 import * as http from 'http';
 import * as WebSocketServer from "./websocket/websocket"
+import * as path from "path"
 
-const app = express();
 
-//initialize a simple http server
-const server = http.createServer(app);
+function setupWebSocketServer(): http.Server<typeof http.IncomingMessage, typeof http.ServerResponse> {
+    const webSocketApp = express();
+    //initialize a simple http server
+    const webSocketServer = http.createServer(webSocketApp);
 
-//initialize the WebSocket server instance
-const connectionManager = WebSocketServer.CreateWebSocketServer(server)
+    //initialize the WebSocket server instance
+    const connectionManager = WebSocketServer.CreateWebSocketServer(webSocketServer)
 
-//start our server
-server.listen(process.env.PORT || 8999, () => {
-    console.log(`Server started on port ${process.env.PORT || 8999}`);
-});
+    //start our server
+    webSocketServer.listen(process.env.SOCKET_PORT || 8999, () => {
+        console.log(`Web Socket started on port ${process.env.SOCKET_PORT || 8999}`);
+    });
+
+    return webSocketServer
+}
+
+function setupHttpServer(): http.Server<typeof http.IncomingMessage, typeof http.ServerResponse> {
+    const httpApp = express();
+    const httpServer = http.createServer(httpApp)
+    
+    httpApp.use(express.static(path.join(__dirname, 'frontendBuild')));
+    httpApp.get('/', function (req, res) {
+        res.sendFile(path.join(__dirname, 'frontendBuild', 'index.html'))
+    });
+    httpApp.listen(process.env.HTTP_PORT || 8080, () => {
+        console.log(`HTTP Server started on port ${process.env.HTTP_PORT || 8080}`);
+    });
+
+    return httpServer
+}
+
+setupWebSocketServer();
+setupHttpServer();
